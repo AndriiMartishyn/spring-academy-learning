be3361c06ce5dc397a67a60f6e0dcbaf577daebf
Andrii Martishyn <contactgently@gmail.com>
2024-04-17 20:33:44 +0300

    Initial structure commit

---
+++ family-cash-card/src/test/java/com/martishyn/familycashcard/FamilyCashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martishyn.familycashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyCashCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
